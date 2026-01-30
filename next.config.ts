457dd081bddf5066d7ea8ef028d74fc29122233f
pedrofalco <fpfalco@gmail.com>
2026-01-30 09:31:43 -0300

    Update ngrok domain

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['static.usernames.app-backend.toolsforhumanity.com'],
   },
-  allowedDevOrigins: ['https://1c093482a017.ngrok-free.app/'], // Add your dev origin here
+  allowedDevOrigins: ['https://enhanced-bull-talented.ngrok-free.app'], // Add your dev origin here
   reactStrictMode: false,
 };
 
