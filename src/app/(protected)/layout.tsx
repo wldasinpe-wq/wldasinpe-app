e4060c4ba10a1ae04ca74c065733d328d769af77
pedrofalco <fpfalco@gmail.com>
2026-04-04 11:16:25 -0300

    style: Ridivi shell theme and PageLayout chrome

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -18,7 +18,7 @@ export default async function TabsLayout({
   return (
     <Page>
       {children}
-      <Page.Footer className="px-0 fixed bottom-0 w-full bg-white">
+      <Page.Footer className="px-0 fixed bottom-0 w-full" style={{ background: 'var(--white-ridivi)' }}>
         <Navigation />
       </Page.Footer>
     </Page>
--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -18,9 +18,10 @@ const Header = (props: { children: ReactNode; className?: string }) => {
   return (
     <header
       className={twMerge(
-        'bg-white flex flex-col justify-center px-6 pt-6 pb-3 z-10',
+        'flex flex-col justify-center px-6 pt-6 pb-3 z-10',
         clsx(props.className),
       )}
+      style={{ background: 'var(--white-ridivi)' }}
     >
       {props.children}
     </header>
@@ -31,7 +32,7 @@ const Main = (props: { children: ReactNode; className?: string }) => {
   return (
     <main
       className={twMerge(
-        clsx('grow overflow-y-auto p-6 pt-3', props.className),
+        clsx('grow overflow-y-auto p-4', props.className),
       )}
     >
       {props.children}
